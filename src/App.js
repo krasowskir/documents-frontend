a2a320cba6adfee3657406b6587219343d79fb88
Richard Krasowski <r.krasowski@tarent.de>
2018-10-24 09:54:44 +0200

    version 3 -tabs fr nav funktionieren -add jquery to dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">
+            Erster Menüeintrag
           </a>
-        </header>
+        </nav>
+        <ul className="nav nav-tabs" id="mytab" role="tablist">
+          <li className="nav-item">
+            <a className="nav-link" id="start-tab" href="#start" data-toggle="tab" role="tab" aria-controls="start" aria-selected="true">
+              Test1
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" id="profil-tab" href="#profil" data-toggle="tab" role="tab" aria-controls="profil" aria-selected="false">
+              Test2
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" id="kontakt-tab" href="#kontakt" data-toggle="tab" role="tab" aria-controls="kontakt" aria-selected="false">
+              Test3
+            </a>
+          </li>
+        </ul>
+
+        <div className="tab-content" id="mytabcontetn">
+          <div className="tab-pane fade show active" id="start" role="tabpanel" aria-labelledby="start-tab">
+            Start
+          </div>
+          <div className="tab-pane fade" id="profil" role="tabpanel" aria-labelledby="profil-tab">
+            Profil
+          </div>
+          <div className="tab-pane fade" id="kontakt" role="tabpanel" aria-labelledby="kontakt-tab">
+            Kontakt
+          </div>
+        </div>
       </div>
     );
   }
